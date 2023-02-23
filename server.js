f531d7d5af3f3c4567c81a10ff0da3965a41272d
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2023-02-23 13:19:49 -0600

    mostly js work and added env example file

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mysql = require('mysql2');
 const express = require('express');
 const cTable = require('console.table');
 
+require('dotenv').config();
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -17,16 +19,12 @@ const db = mysql.createConnection(
   {
     host: '127.0.0.1',
     user: 'root',
-    password: '',
+    password: process.env.db_password,
     database: 'workplace_db'
   },
   console.log(`Connected to the workplace_db database.`)
 );
 
-
-// let Depts =[]
-// let Roles = []
-// let Employees = []
 
 const promptUser = async () => {
     const data = await inquirer
@@ -36,69 +34,146 @@ const promptUser = async () => {
         type: 'list',
         name: 'options',
         message: 'Select an action:',
-        choices: ['View All Depts','View All Roles','View All Employees', 'Add A Dept', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
+        choices: ['View All Depts','View All Roles','View All Employees', 'Add a Dept', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
       },
-      {
-        type: 'input',
-        name: 'dept',
-        message: 'Please Enter Department Name:',
-        when: (Depts) => Depts.options === 'Add A Dept',
-      },
+      // {
+      //   type: 'input',
+      //   name: 'dept',
+      //   message: 'Please Enter Department Name:',
+      //   when: (Depts) => Depts.options === 'Add A Dept',
+      // },
   
-      {
-        type: 'input',
-        name: 'role',
-        message: 'Please Enter Role Name:',
-        when: (Roles) => Roles.options === 'Add a Role',
-      },
+      // {
+      //   type: 'input',
+      //   name: 'role',
+      //   message: 'Please Enter Role Name:',
+      //   when: (Roles) => Roles.options === 'Add a Role',
+      // },
   
-      {
-        type: 'input',
-        name: 'employee',
-        message: 'Please Enter Employee Full Name:',
-        when: (Employees) => Employees.options === 'Add an Employee',
-      },
+      // {
+      //   type: 'input',
+      //   name: 'employee',
+      //   message: 'Please Enter Employee Full Name:',
+      //   when: (Employees) => Employees.options === 'Add an Employee',
+      // },
     ])
 
     if (data.options === "View All Depts") {
         // show dept table including dept names and ids
         db.query('SELECT * FROM department', function (err, results) {
           console.table(results);
+          promptUser();
         });
         ///
       } else if (data.options === "View All Roles") {
         // show roles table including job title, role id, dept, salary
         db.query('SELECT * FROM role', function (err, results) {
           console.table(results);
+          promptUser();
         });
 
       } else if (data.options === "View All Employees") {
         //show employees table including employee ids, first names, last names, job titles, departments, salaries, and managers
         db.query('SELECT * FROM employee', function (err, results) {
           console.table(results);
+          promptUser();
         });
 
       } else if (data.options === "Add a Dept") {
-        Depts.push(data.dept)
-        console.log(Depts)
+        inquirer.prompt( [
+          {
+            type: 'input',
+            name: 'deptName',
+            message: 'Please Enter Department Name:',
+          }
+        ])
+        .then(response => {
+          db.query(
+            'INSERT INTO department (name) VALUE (?)', 
+          [response.deptName],
+          function (err, res) {
+            if (err) throw err;
+            console.log(`${response.deptName} has been added to the department table!`);
+            promptUser();
+            }
+        )
+       });
 
       } else if (data.options === "Add a Role") {
-        Roles.push(data.role)
-        console.log(Roles)
-
-      } else if (data.options === "Add an Employee") {
-        Employees.push(data.employee)
-        console.log(Employees)
-      } 
+        inquirer.prompt( [
+          {
+            type: 'input',
+            name: 'roleTitle',
+            message: 'Please Enter Title of Role:',
+          },
+          {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'Please Enter Salary of Role:',
+          },
+          {
+            type: 'input',
+            name: 'roleDeptID',
+            message: 'Please Enter Department ID of Role:',
+          }
+        ])
+        .then(response => {
+        db.query(
+          'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', 
+          [response.roleTitle, response.roleSalary, response.roleDeptID],
+          function (err, res) {
+          if (err) throw err;
+          console.log(`The ${response.roleTitle} role has been added to the role table with a salary of $${response.roleSalary} and department ID of ${response.roleDeptID}!`);
+          promptUser();
+          }
+        )
+      })
+    }
+        
+    else if (data.options === "Add an Employee") {
+        inquirer.prompt( [
+          {
+            type: 'input',
+            name: 'firstName',
+            message: 'Please Enter Employees First Name:',
+          },
+          {
+            type: 'input',
+            name: 'lastName',
+            message: 'Please Enter Employees Last Name:',
+          },
+          {
+            type: 'input',
+            name: 'roleId',
+            message: 'Please Enter Employees Role ID:',
+          },
+          {
+            type: 'input',
+            name: 'managerID',
+            message: 'Please Enter Employee Managers ID:',
+          }
+        ])
+        .then(response => {
+        db.query(
+        'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)',
+        [response.firstName, response.lastName, response.roleId, response.managerID],
+        function (err, res) {
+          if (err) throw err;
+          console.log(`${response.firstName} ${response.lastName} has been added to the employee table!`);
+          promptUser(); 
+      })
+  })
+}
 }
 
 promptUser()
 
 
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
 
 
 //inquirer prompt 
@@ -118,4 +193,3 @@ app.listen(PORT, () => {
 // if user selects option to add an employee: they are prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 
 // if user selects option to update an employee role: they are prompted to select an employee to update and their new role and this information is updated in the database
-
