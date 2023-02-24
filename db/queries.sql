fb812485bb846f7479649adb5750151eabaa6c58
lsieck519 <99048123+lsieck519@users.noreply.github.com>
2023-02-24 11:43:56 -0600

    added better queries

--- db/queries.sql
+++ db/queries.sql
@@ -1,13 +1,28 @@
+-- for department query in server.js
+SELECT 
+    d.id as "Dept ID", 
+    d.name as "Department" 
+FROM department d 
+ORDER BY d.id ASC
 
-SELECT
-    e.id,
-    concat(e.first_name,' ',e.last_name) as employee_name, 
-    r.title,
-    d.name as department,
-    r.salary,
-    e.manager_id
-FROM employee e
-LEFT JOIN role r
-on r.id = e.role_id
-LEFT JOIN department d
-on d.id = r.department_id
+-- for role query in server.js
+SELECT 
+    r.title as "Job Title", 
+    r.id as "Role ID", 
+    d.name as "Department", 
+    r.salary as "Salary" 
+FROM role r 
+JOIN department d ON r.department_id = d.id 
+ORDER BY r.title ASC
+
+-- for employee query in server.js
+SELECT 
+    e.id as "Employee ID", 
+    CONCAT(e.first_name," ", e.last_name) as "Employee Name", 
+    r.title as "Title", d.name as "Department", 
+    r.salary as "Salary", 
+    e.manager_id as "Manager ID" 
+FROM employee e 
+LEFT JOIN role r ON r.id = e.role_id 
+LEFT JOIN department d ON d.id = r.department_id 
+ORDER BY e.id ASC
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const inquirer = require("inquirer")
+const inquirer = require('inquirer')
 const fs = require('fs')
 const mysql = require('mysql2');
 const express = require('express');
@@ -9,7 +9,6 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -22,7 +21,7 @@ const db = mysql.createConnection(
     password: process.env.db_password,
     database: 'workplace_db'
   },
-  console.log(`Connected to the workplace_db database.`)
+  console.log(`Successfully connected to the workplace_db database!`)
 );
 
 
@@ -34,48 +33,43 @@ const promptUser = async () => {
       {
         type: 'list',
         name: 'options',
-        message: 'Select an action:',
+        message: 'Select an Action:',
         choices: ['View All Depts','View All Roles','View All Employees', 'Add a Dept', 'Add a Role', 'Add an Employee', 'Update an Employee'],
-      },
-      // {
-      //   type: 'input',
-      //   name: 'dept',
-      //   message: 'Please Enter Department Name:',
-      //   when: (Depts) => Depts.options === 'Add A Dept',
-      // },
-  
-      // {
-      //   type: 'input',
-      //   name: 'role',
-      //   message: 'Please Enter Role Name:',
-      //   when: (Roles) => Roles.options === 'Add a Role',
-      // },
-  
-      // {
-      //   type: 'input',
-      //   name: 'employee',
-      //   message: 'Please Enter Employee Full Name:',
-      //   when: (Employees) => Employees.options === 'Add an Employee',
-      // },
+      }
     ])
-
+  
     if (data.options === "View All Depts") {
-        // show dept table including dept names and ids
-        db.query('SELECT * FROM department', function (err, results) {
+        // show department table including dept names and ids
+        db.query(
+        'SELECT d.id as "Dept ID", d.name as "Department" \
+        FROM department d ORDER BY d.id ASC', 
+        function (err, results) {
           console.table(results);
           promptUser();
         });
         ///
       } else if (data.options === "View All Roles") {
-        // show roles table including job title, role id, dept, salary
-        db.query('SELECT * FROM role', function (err, results) {
+        // show role table including job title, role id, dept, salary
+        db.query(
+        'SELECT r.title as "Job Title", r.id as "Role ID", d.name as "Department", r.salary as "Salary" \
+        FROM role r \
+        JOIN department d on r.department_id = d.id \
+        ORDER BY r.title ASC', 
+        function (err, results) {
           console.table(results);
           promptUser();
         });
 
       } else if (data.options === "View All Employees") {
         //show employees table including employee ids, first names, last names, job titles, departments, salaries, and managers
-        db.query('SELECT * FROM employee', function (err, results) {
+        db.query(
+        'SELECT e.id as "Employee ID", CONCAT(e.first_name," ", e.last_name) as "Employee Name", \
+                r.title as "Title", d.name as "Department", r.salary as "Salary", e.manager_id as "Manager ID" \
+        FROM employee e \
+        LEFT JOIN role r on r.id = e.role_id \
+        LEFT JOIN department d on d.id = r.department_id \
+        ORDER BY e.id ASC', 
+        function (err, results) {
           console.table(results);
           promptUser();
         });
@@ -85,7 +79,7 @@ const promptUser = async () => {
           {
             type: 'input',
             name: 'deptName',
-            message: 'Please Enter Department Name:',
+            message: 'Enter Department Name:',
           }
         ])
         .then(response => {
@@ -103,17 +97,17 @@ const promptUser = async () => {
           {
             type: 'input',
             name: 'roleTitle',
-            message: 'Please Enter Title of Role:',
+            message: 'Enter Title of the Role:',
           },
           {
             type: 'input',
             name: 'roleSalary',
-            message: 'Please Enter Salary of Role:',
+            message: 'Enter Salary of the Role',
           },
           {
             type: 'input',
             name: 'roleDeptID',
-            message: 'Please Enter Department ID of Role:',
+            message: 'Enter Department ID of Role:',
           }
         ])
         .then(response => {
@@ -122,7 +116,8 @@ const promptUser = async () => {
           [response.roleTitle, response.roleSalary, response.roleDeptID],
           function (err, res) {
           if (err) throw err;
-          console.log(`The ${response.roleTitle} role has been added to the role table with a salary of $${response.roleSalary} and department ID of ${response.roleDeptID}!`);
+          console.log(`The ${response.roleTitle} role has been added to the role table with a salary of $${response.roleSalary}\
+          and department ID of ${response.roleDeptID}!`);
           promptUser();
           })
       });
@@ -131,22 +126,22 @@ const promptUser = async () => {
           {
             type: 'input',
             name: 'firstName',
-            message: 'Please Enter Employees First Name:',
+            message: 'Enter Employees First Name:',
           },
           {
             type: 'input',
             name: 'lastName',
-            message: 'Please Enter Employees Last Name:',
+            message: 'Enter Employees Last Name:',
           },
           {
             type: 'input',
             name: 'roleId',
-            message: 'Please Enter Employees Role ID:',
+            message: 'Enter Employees Role ID:',
           },
           {
             type: 'input',
             name: 'managerID',
-            message: 'Please Enter Employee Managers ID:',
+            message: 'Enter Employee Managers ID (press ENTER if none):',
           }
         ])
         .then(response => {
@@ -189,7 +184,7 @@ const promptUser = async () => {
   ])
   .then(response => {
   db.query(
-  'UPDATE employee (first_name, last_name, role_id, manager_id) SET (?, ?, ?, ?) WHERE employee.id = ?',
+  'UPDATE employee (first_name, last_name, role_id, manager_id) SET (?, ?, ?, ?) WHERE id = ?',
   [response.firstName, response.lastName, response.roleID, response.managerID, response.empID],
   function (err, res) {
     if (err) throw err;
@@ -207,26 +202,5 @@ const promptUser = async () => {
 promptUser()
 
 
-
-
-
-
-
-
-//inquirer prompt 
-
-    //options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-
-//if user selects view all dept: formatted table showing department names and department ids will appear
-
-// if user selects view all roles: formatted table showing job title, role id, the department that role belongs to, and the salary for that role will appear
-
-// if user selects view all employees: formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-
-// if user selects option to to add a department: they are prompted to enter the name of the department and that department is added to the database
-
-// if user selects option to add a role: they are prompted to enter the name, salary, and department for the role and that role is added to the database
-
-// if user selects option to add an employee: they are prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 
 // if user selects option to update an employee role: they are prompted to select an employee to update and their new role and this information is updated in the database
